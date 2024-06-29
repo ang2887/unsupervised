1980fc8b24a31d8b7dc9adb28df1f7e50de0ee22
Lena <ang2887@gmail.com>
2024-06-29 11:37:53 +0100

    Update preprocess module and associated cache files

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -3,7 +3,7 @@ from impute import impute_drop_missing_values
 from scale import robust_scale
 from outliers import windsorize
 
-df = pd.read_csv('../CC_GENERAL.csv',index_col='CUST_ID')
+df = pd.read_csv('/Users/alenadocherty/Downloads/PROJECTS/Unsupervised/CC GENERAL.csv',index_col='CUST_ID')
 
 df.columns = [col.lower() for col in df.columns]
 
